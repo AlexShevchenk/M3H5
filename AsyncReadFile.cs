4ee814085099b2fe9c5170f774e941b2011b074d
Oleksandr Shevchenko <105636389+AlexShevchenk@users.noreply.github.com>
2023-03-20 16:50:55 +0200

    File about async

---
+++ AsyncReadFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+namespace M3H5
+{
+	public class AsyncReadFile
+	{
+		public static async Task<string> ReadHello(string path)
+		{
+			using Task<string> task = File.ReadAllTextAsync(path);
+			await task;
+			return task.Result;
+		}
+		public static async  Task<string> ReadWorld(string path)
+		{
+			using Task<string> task = File.ReadAllTextAsync(path);
+			await task;
+			return task.Result;
+		}
+		public static async Task<string> Concat(string path1,string path2)
+		{
+			var str1 = await ReadHello(path1);
+			var str2 = await ReadWorld(path2);
+			return str1 + " " + str2;
+		}
+
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace M3H5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            var hellloWorld = AsyncReadFile.Concat("Hello.txt", "World.txt");
+            Console.WriteLine(hellloWorld.Result);
+        }
+
+
+    }
+}
